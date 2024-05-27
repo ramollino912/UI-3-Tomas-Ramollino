0c8cae790dfea07dfe0eb8f6632d8b16cbac8ebf
ramollino912 <ramonaramollino@gmail.com>
2024-05-27 15:52:57 -0300

    ui 3

---
+++ Assets/lol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lol : MonoBehaviour
+{
+    public InputField contraseña;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void contras()
+    {
+        if(contraseña.text == "borja")
+        {
+            Debug.Log("acces granted");
+
+        }
+        else
+        {
+            Debug.Log("acces denied");
+        }
+    }
+}
